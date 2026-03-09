b8d231d2dea355b47df744be64a35be21001acbc
Gilberto <gil79430@gmail.com>
2026-03-08 23:01:15 -0300

    feat(api): o backend agora recebe informações do formulário no frontend

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,22 @@ const port = 3000;
 
 // permite que a API receba e entenda JSON
 app.use(express.json());
+// permite que o express leia dados enviados por formulários
+app.use(express.urlencoded({ extended: true }));
 
 // servindo arquivos estáticos
-
 // como estamos usando o ES Modules ("type": "module"; no package.json), precisamos usar "import.meta.dirname" no lugar do "__dirname"
-
 // ao especificar o caminho relativo da pasta onde estão os arquivos do frontend, precisamos separar cada etapa do caminho com vírgulas
 app.use("/", express.static(path.join(import.meta.dirname, "../", "frontend", "public")));
+
+// método POST para receber as informações vindas do formulário no frontend
+// a URL no primeiro parâmetro precisa ser a mesma indicada no atributo "action" da tag "form"
+app.post("/", (req, res) => {
+    // esse destructuring precisa ter as variáveis iguais aos valores dos atributos "name" nos <input> do HTML
+    let { tarefa, descricao } = req.body;
+    console.log(tarefa, descricao);
+    res.send("Dados recebidos");
+});
 
 // iniciando servidor
 app.listen(port, () => {
