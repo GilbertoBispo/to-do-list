2c5e2f2675a628dfeda83d1b17994bc68ed74d0d
Gilberto <gil79430@gmail.com>
2026-03-11 14:56:49 -0300

    fix: o evento de click enviado pro backend agora envia os IDs individuais de cada botão, ao invés de enviar sempre o mesmo

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -12,7 +12,7 @@ async function carregarTarefas() {
                 <details class='toggle'>
                     <summary>
                     ${task.titulo}
-                        <button>
+                        <button id='${task.id}'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ed736b" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -33,16 +33,20 @@ async function carregarTarefas() {
 
 carregarTarefas()
 
+deleteTask();
 // função para enviar evento de click no botão de excluir
-async function deleteTask() {
-    await carregarTarefas();
+function deleteTask() {
     
     try {
-        $("summary > button").on("click", () => {
-            const clique = {
-                mensagem: "Click"
-            }
+        $(document).on("click", "summary > button", function() {
             
+            let idTask = $(this).attr("id");
+
+            const clique = {
+                mensagem: "Click",
+                idTask
+            };
+            // envia evento pro backend através do objeto "clique"
             $.ajax({
                 url: "http://localhost:3000/deleteTask",
                 type: "POST",
@@ -52,7 +56,7 @@ async function deleteTask() {
                     console.log("Sucesso, ", response);
                 },
                 error: function(err) {
-                    console.log("Erro", err)
+                    console.log("Erro", err);
                 }
             });
         });
@@ -61,5 +65,3 @@ async function deleteTask() {
         console.log(e);
     }
 }
-
-deleteTask()
