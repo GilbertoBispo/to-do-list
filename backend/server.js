5c3801c90a66ac9ef28ab4c2d5af4a57133f0ba4
Gilberto <gil79430@gmail.com>
2026-03-20 13:13:24 -0300

    fix: resolve o erro de chamadas de rotas do router.js no server.js

--- backend/server.js
+++ backend/server.js
@@ -14,22 +14,23 @@ app.use(express.json());
 // permite que o express leia dados enviados por formulários
 app.use(express.urlencoded({ extended: true }));
 
+// rotas
+app.use("/", router)
 // servindo arquivos estáticos
 // como estamos usando o ES Modules ("type": "module"; no package.json), precisamos usar "import.meta.dirname" no lugar do "__dirname"
 // ao especificar o caminho relativo da pasta onde estão os arquivos do frontend, precisamos separar cada etapa do caminho com vírgulas
 app.use("/", express.static(path.join(import.meta.dirname, "../", "frontend", "public")));
 
 
-// rotas
-app.use("/", router)
+/*
 app.use("/api/tarefas", router)
 app.use("/api/deleteTask/:id", router)
 app.use("/api/editTask/:id", router)
+*/
 
-
-// iniciando servidor
+/* iniciando servidor
 app.listen(port, () => {
     console.log("executando servidor.");
 });
-
+*/
 export default app;
