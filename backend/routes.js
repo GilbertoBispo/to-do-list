a47eb91a85d162c46bf21a80f34d1f8dad65cba7
Gilberto <gil79430@gmail.com>
2026-03-22 19:46:19 -0300

    fix: corrige rota especificada no fetch do arquivo 'script'

--- backend/routes.js
+++ backend/routes.js
@@ -4,7 +4,7 @@ import queryTasks from "./funcoes.js";
 const router = express.Router();
 
 // rota GET para exibir tarefas numa div no frontend
-router.get("/api/tarefas", async (req, res) => {
+router.get("/tarefas", async (req, res) => {
     try {
         const tarefas = await queryTasks();
         res.send(tarefas);
