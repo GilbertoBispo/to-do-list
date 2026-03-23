4e89a022f138740889768bee10949c38bdee579f
Gilberto <gil79430@gmail.com>
2026-03-22 21:24:04 -0300

    fix: remove o app.use dos arquivos estáticos do server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api", router);
 // servindo arquivos estáticos
 // como estamos usando o ES Modules ("type": "module"; no package.json), precisamos usar "import.meta.dirname" no lugar do "__dirname"
 // ao especificar o caminho relativo da pasta onde estão os arquivos do frontend, precisamos separar cada etapa do caminho com vírgulas
-app.use(express.static(path.join(import.meta.dirname, "../", "frontend", "public")));
+//app.use(express.static(path.join(import.meta.dirname, "../", "frontend", "public")));
 
 app.get("/api/debug", (req, res) => {
   res.send("debug funcionando");
